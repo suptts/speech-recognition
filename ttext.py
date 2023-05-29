90f81a336a3947732ef4ce5fa1b7d49e1fd2d53a
suptts <134623522+suptts@users.noreply.github.com>
2023-05-29 19:25:22 +0700

    Update ttext.py

--- ttext.py
+++ ttext.py
@@ -3,9 +3,11 @@
 import goslate
 
 text = "Hello World"
+#text = input("Please enter a string:\n")
 
 gs = goslate.Goslate()
 translatedText = gs.translate(text, "fr")
 # translatedText = gs.translate(text, "ja")
 
 print(translatedText)
+#print(f'You entered {text} and in French  is {gs.translate(text, "fr")}')
